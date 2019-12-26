dbd96f6190bff81cd246522dff94a7112d89d983
mohsinonxrm <mohsinonxrm@outlook.com>
2019-12-26 14:30:50 -0800

    Add project files.

---
+++ D365WebAPIWithPolly/D365WebAPIService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D365WebAPIWithPolly
+{
+    public class D365WebAPIService : IHostedService
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public D365WebAPIService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await WhoAmIRequestAsync();
+        }
+
+        public async Task WhoAmIRequestAsync()
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient("D365WebApiClient");
+            var response = await httpClient.GetAsync("WhoAmI");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                JObject result = JsonConvert.DeserializeObject(json) as JObject;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Display(result);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Failed to call the Web Api: {response.StatusCode}");
+                string content = await response.Content.ReadAsStringAsync();
+
+                // Note that if you got reponse.Code == 403 and reponse.content.code == "Authorization_RequestDenied"
+                // this is because the tenant admin as not granted consent for the application to call the Web API
+                Console.WriteLine($"Content: {content}");
+            }
+        }
+        private static void Display(JObject result)
+        {
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ D365WebAPIWithPolly/Program.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Client;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Registry;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace D365WebAPIWithPolly
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await RunAsync();//.GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+
+            // You can run this sample using ClientSecret or Certificate. The code will differ only when instantiating the IConfidentialClientApplication
+            bool isUsingClientSecret = AppUsesClientSecret(config);
+
+            // Even if this is a console application here, a daemon application is a confidential client application
+            IConfidentialClientApplication app;
+
+            if (isUsingClientSecret)
+            {
+                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                    .WithClientSecret(config.ClientSecret)
+                    .WithAuthority(new Uri(config.Authority))
+                    .Build();
+            }
+
+            else
+            {
+                X509Certificate2 certificate = ReadCertificate(config.CertificateName);
+                app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                    .WithCertificate(certificate)
+                    .WithAuthority(new Uri(config.Authority))
+                    .Build();
+            }
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator
+            string[] scopes = new string[] { "https://{CRM_URL}.crm.dynamics.com/.default" };
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(scopes)
+                    .ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token acquired");
+                Console.ResetColor();
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+            {
+                // Invalid scope. The scope has to be of the form "https://resourceurl/.default"
+                // Mitigation: change the scope to be as expected
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Scope provided is not supported");
+                Console.ResetColor();
+            }
+
+            if (result != null)
+            {
+                var builder = new HostBuilder().ConfigureServices((hostContext, services) =>
+                {
+                    // Create wait and retry policy that will retry up to 3 times and use exponential backoff as default.
+                    // However, if Retry-After is provided then that will be used instead
+                    IAsyncPolicy<HttpResponseMessage> waitAndRetryPolicy = HttpPolicyExtensions
+                                  .HandleTransientHttpError() // HttpRequestException, 5XX and 408
+                                  .OrResult(response => (int)response.StatusCode == 429) // RetryAfter
+                                  .WaitAndRetryAsync(
+                                    retryCount: 3,
+                                    sleepDurationProvider: (retryAttempt, context) =>
+                                    {
+                                        var retryAfter = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)); //default exponential backoff
+
+                                        if (context == null || !context.ContainsKey("RetryAfter"))
+                                        {
+                                            return retryAfter;
+                                        }
+                                        else
+                                        {
+                                            return TimeSpan.FromSeconds(Convert.ToInt32(context["RetryAfter"]));
+                                        }
+                                    },
+                                    onRetryAsync: (exception, timespan, retryAttempt, context) =>
+                                    {
+                                        Console.WriteLine($"Retry Attempt No: {retryAttempt}");
+                                        return Task.CompletedTask;
+                                    });
+
+                    // Create a noOp policy, so that can be used for idempotent requests
+                    IAsyncPolicy<HttpResponseMessage> noOpPolicy = Policy.NoOpAsync().AsAsyncPolicy<HttpResponseMessage>();
+
+                    // Create policy registry and add the policies to it
+                    var registry = new PolicyRegistry()
+                                    {
+                                        { "WaitAndRetryPolicy", waitAndRetryPolicy },
+                                        { "NoOpPolicy", noOpPolicy },
+                                    };
+                    
+                    services.AddPolicyRegistry(registry);
+                    
+                    services.AddHttpClient("D365WebApiClient", client =>
+                    {
+                        client.BaseAddress = new Uri("https://{CRM_URL}.crm.dynamics.com/api/data/v9.1/");
+                        client.DefaultRequestHeaders.Add("Accept", "application/json");
+                        client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+                        client.DefaultRequestHeaders.Add("OData-Version", "4.0");
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
+                    }).AddPolicyHandlerFromRegistry((policyRegistry, httpRequestMessage) =>
+                    {
+                        // Use Wait and Retry policy for GET and DELETE only
+                        if (httpRequestMessage.Method == HttpMethod.Get || httpRequestMessage.Method == HttpMethod.Delete)
+                        {
+                            return policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>("WaitAndRetryPolicy");
+                        }
+
+                        // Use NoOp Policy for all other HTTP verbs
+                        return policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>("NoOpPolicy");
+                    });
+
+                    services.AddSingleton<IHostedService, D365WebAPIService>();
+                });
+
+                await builder.RunConsoleAsync();
+            }
+        }
+
+        /// <summary>
+        /// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
+        /// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
+        /// </summary>
+        /// <param name="config">Configuration from appsettings.json</param>
+        /// <returns></returns>
+        private static bool AppUsesClientSecret(AuthenticationConfig config)
+        {
+            string clientSecretPlaceholderValue = "[Enter here a client secret for your application]";
+            string certificatePlaceholderValue = "[Or instead of client secret: Enter here the name of a certificate (from the user cert store) as registered with your application]";
+
+            if (!String.IsNullOrWhiteSpace(config.ClientSecret) && config.ClientSecret != clientSecretPlaceholderValue)
+            {
+                return true;
+            }
+
+            else if (!String.IsNullOrWhiteSpace(config.CertificateName) && config.CertificateName != certificatePlaceholderValue)
+            {
+                return false;
+            }
+
+            else
+                throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
+        }
+
+        private static X509Certificate2 ReadCertificate(string certificateName)
+        {
+            if (string.IsNullOrWhiteSpace(certificateName))
+            {
+                throw new ArgumentException("certificateName should not be empty. Please set the CertificateName setting in the appsettings.json", "certificateName");
+            }
+            X509Certificate2 cert = null;
+
+            using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certCollection = store.Certificates;
+
+                // Find unexpired certificates.
+                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+
+                // From the collection of unexpired certificates, find the ones with the correct name.
+                X509Certificate2Collection signingCert = currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certificateName, false);
+
+                // Return the first certificate in the collection, has the right name and is current.
+                cert = signingCert.OfType<X509Certificate2>().OrderByDescending(c => c.NotBefore).FirstOrDefault();
+            }
+            return cert;
+        }
+    }
+}
